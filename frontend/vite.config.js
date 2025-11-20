af9be515980b7e8e2c85c170f902aea4f719de11
Orange-Juice03 <alvaroe.glz03@gmail.com>
2025-11-20 02:38:43 -0600

    changes

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      // Matches any request starting with /api
+      '/api': {
+        target: 'http://127.0.0.1:5000', // YOUR SERVER PORT HERE
+        changeOrigin: true,
+        secure: false,
+        // Optional: Rewrite removes '/api' before sending to backend
+        // rewrite: (path) => path.replace(/^\/api/, ''), 
+      },
+    },
+  },
+})
